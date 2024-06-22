343e5a7078f7fdb400657e612df7eba3674d0877
HannaWang003 <hannawang003@gmail.com>
2024-06-23 00:04:33 +0800

    done:完成最新文章的顯示及刪除功能

---
+++ api/edit_news.php
@@ -0,0 +1,16 @@
+<?php
+include_once "db.php";
+foreach ($_POST['id'] as $id) {
+    if (isset($_POST['del']) && in_array($id, $_POST['del'])) {
+        $News->del($id);
+    } else {
+        $row = $News->find($id);
+        if (isset($_POST['sh']) && in_array($id, $_POST['sh'])) {
+            $row['sh'] = 1;
+        } else {
+            $row['sh'] = 0;
+        }
+        $News->save($row);
+    }
+}
+to("../back.php?do=news");
--- back/news.php
+++ back/news.php
@@ -7,19 +7,19 @@ $start = ($now - 1) * $div;
 $news = $News->all("limit $start,$div");
 ?>
 <style>
-table {
-    width: 90%;
-    margin: auto;
-    text-align: center;
-}
+    table {
+        width: 90%;
+        margin: auto;
+        text-align: center;
+    }
 
-td:nth-child(1),
-td:nth-child(3),
-td:nth-child(4) {
-    width: 10%;
-}
+    td:nth-child(1),
+    td:nth-child(3),
+    td:nth-child(4) {
+        width: 10%;
+    }
 </style>
-<form action="./api/edit_news.php" post="post">
+<form action="./api/edit_news.php" method="post">
     <table>
         <tr>
             <th>編號</th>
@@ -30,12 +30,13 @@ td:nth-child(4) {
         <?php
         foreach ($news as $key => $n) {
         ?>
-        <tr>
-            <td class="clo"><?= $key + $start + 1 ?>.</td>
-            <td><?= $n['title'] ?></td>
-            <td><input type="checkbox" name="sh[]" value="<?= $n['id'] ?>" <?= ($n['sh'] == 1) ? "checked" : "" ?>></td>
-            <td><input type="checkbox" name="del[]" value="<?= $n['id'] ?>"></td>
-        </tr>
+            <tr>
+                <td class="clo"><?= $key + $start + 1 ?>.</td>
+                <td><?= $n['title'] ?></td>
+                <td><input type="checkbox" name="sh[]" value="<?= $n['id'] ?>" <?= ($n['sh'] == 1) ? "checked" : "" ?>></td>
+                <td><input type="checkbox" name="del[]" value="<?= $n['id'] ?>"></td>
+            </tr>
+            <input type="hidden" name="id[]" value="<?= $n['id'] ?>">
         <?php
         }
         ?>
@@ -43,19 +44,20 @@ td:nth-child(4) {
     <div class="ct">
         <?php
         if (($now - 1) > 0) {
-            $pre = $now-1;
+            $pre = $now - 1;
             echo "<a href='?do=news&p=$pre'><</a>";
         }
         $style = "style='font-size:20px;'";
         for ($i = 1; $i <= $pages; $i++) {
         ?>
-        <a href="?do=news&p=<?= $i ?>" <?= ($now == $i) ? $style : "" ?>><?= $i ?></a>
+            <a href="?do=news&p=<?= $i ?>" <?= ($now == $i) ? $style : "" ?>><?= $i ?></a>
         <?php
         }
         if (($now + 1) <= $pages) {
-        $next = $now+1;
+            $next = $now + 1;
             echo "<a href='?do=news&p=$next'>></a>";
         }
         ?>
     </div>
+    <div class="ct"><input type="submit" value="確定修改"></div>
 </form>
