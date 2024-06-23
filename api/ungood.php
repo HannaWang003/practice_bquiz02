1ba52dffa2fdcf8054606ce13240fb301233dae8
HannaWang003 <hannawang003@gmail.com>
2024-06-23 09:36:50 +0800

    done:完成人氣文章讚數功能

--- api/ungood.php
+++ api/ungood.php
@@ -1,3 +1,6 @@
 <?php
 include_once "db.php";
 $Log->del(['acc' => $_SESSION['user'], 'news' => $_POST['id']]);
+$n = $News->find($_POST['id']);
+$n['good']--;
+$News->save($n);
---
+++ front/pop.php
@@ -0,0 +1,84 @@
+<?php
+$total = $News->count(['sh' => 1]);
+$div = 5;
+$pages = ceil($total / $div);
+$now = ($_GET['p']) ?? 1;
+$start = ($now - 1) * $div;
+$news = $News->all(['sh' => 1], "order by `good` desc limit $start,$div");
+?>
+<fieldset>
+    <legend>目前位置:首頁 > 人氣文章區</legend>
+    <table style="width:95%;margin:auto">
+        <tr>
+            <th style="width:30%">標題</th>
+            <th>內容</th>
+            <th>人氣</th>
+        </tr>
+        <?php
+        foreach ($news as $n) {
+        ?>
+        <tr>
+            <td class="clo title" data-id="<?= $n['id'] ?>"><?= $n['title'] ?></td>
+            <td id="s<?= $n['id'] ?>"><?= mb_substr($n['news'], 0, 20) ?>...</td>
+            <td id="a<?= $n['id'] ?>" style="display:none;" class="news"><?= $n['news'] ?></td>
+            <td>
+                <?php
+                    echo $n['good'] . "個人說<img src='./img/02B03.jpg' style='width:30px;'>";
+                    if (isset($_SESSION['user'])) {
+                        $good = $Log->find(['acc' => $_SESSION['user'], 'news' => $n['id']]);
+                        echo "<th>";
+                        if ($good <= 0) {
+                            echo "<a onclick='good({$n['id']})'>讚</a>";
+                        } else {
+                            echo "<a onclick='ungood({$n['id']})'>收回讚</a>";
+                        }
+                        echo "</th>";
+                    }
+                    ?>
+            </td>
+        </tr>
+        <?php
+        }
+        ?>
+    </table>
+    <div>
+        <?php
+        if (($now - 1) > 0) {
+            $pre = $now - 1;
+            echo "<a href='?do=pop&p=$pre'><</a>";
+        }
+        $style = "style='font-size:20px;'";
+        for ($i = 1; $i <= $pages; $i++) {
+        ?>
+        <a href="?do=pop&p=<?= $i ?>" <?= ($now == $i) ? $style : "" ?>><?= $i ?></a>
+        <?php
+        }
+        if (($now + 1) <= $pages) {
+            $next = $now + 1;
+            echo "<a href='?do=pop&p=$next'>></a>";
+        }
+        ?>
+    </div>
+</fieldset>
+<script>
+$('.title').on('click', function() {
+    let id = $(this).data('id');
+    $(`#s${id},#a${id}`).toggle();
+})
+
+function good(id) {
+    $.post('./api/good.php', {
+        id
+    }, (res) => {
+        location.href = "?do=pop";
+    })
+}
+
+function ungood(id) {
+    $.post('./api/ungood.php', {
+        id
+    }, (res) => {
+        location.href = "?do=pop";
+    })
+}
+</script>
