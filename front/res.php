a768e338dfc0fe6967d9b28efa3347800c60b4da
HannaWang003 <hannawang003@gmail.com>
2024-06-23 17:30:12 +0800

    done:投票結果完成

--- front/res.php
+++ front/res.php
@@ -1,6 +1,22 @@
 <?php
-$subject = $Que->find($_GET['id'])['text'];
+$subject = $Que->find($_GET['id']);
+$opts = $Que->all(['subject_id' => $_GET['id']]);
 ?>
 <fieldset>
-    <legend>目前位置:首頁>問卷調查><?= $subject ?></legend>
+    <legend>目前位置:首頁>問卷調查><?= $subject['text'] ?></legend>
+    <div><b><?= $subject['text'] ?></b></div>
+    <?php
+    foreach ($opts as $opt) {
+        $rate = round($opt['vote'] / $subject['vote'], 2);
+    ?>
+        <div style="margin:10px 0;">
+            <span style="display:inline-block;width:25%"><?= $opt['text'] ?></span>
+            <span style="display:inline-block;height:30px;background:#ccc;width:<?= 500 * $rate ?>px;"></span>
+            <span><?= $opt['vote'] ?>票(<?= $rate * 100 ?>%)</span>
+        </div>
+
+    <?php
+    }
+    ?>
+    <div class="ct"><button onclick="history.go(-1)">返回</button></div>
 </fieldset>
